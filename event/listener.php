e8874aea97f995e92acbd5ab42050504458187de
Matt Friedman <maf675@gmail.com>
2014-07-01 18:06:14 -0700

    Add empty event listener starter file

---
+++ event/listener.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* Google Analytics extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbb\googleanalytics\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+}
