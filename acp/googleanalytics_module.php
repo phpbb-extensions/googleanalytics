9fc84e6c394d418c916dcd45eec5130f4c3cc681
Michael O'Toole <phpbbireland@gmail.com>
2014-07-02 01:39:35 +0100

    Missed one edit...

--- acp/googleanalytics_module.php
+++ acp/googleanalytics_module.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* Board Rules extension for the phpBB Forum Software package.
+* Google Analytics extension for the phpBB Forum Software package.
 *
 * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
@@ -45,65 +45,6 @@ class googleanalytics_module
 			break;
 
 			case 'manage':
-				// Load a template from adm/style for our ACP page
-				$this->tpl_name = 'googleanalytics_manage';
-
-				// Set the page title for our ACP page
-				$this->page_title = $user->lang('ACP_GOOGLEANALYTICS_MANAGE');
-
-				// Perform any actions submitted by the user
-				switch($action)
-				{
-					case 'add':
-						// Set the page title for our ACP page
-						$this->page_title = $user->lang('ACP_GOOGLEANALYTICS_CREATE_RULE');
-
-						// Load the add rule handle in the admin controller
-						$admin_controller->add_rule($language, $parent_id);
-
-						// Return to stop execution of this script
-						return;
-					break;
-
-					case 'edit':
-						// Set the page title for our ACP page
-						$this->page_title = $user->lang('ACP_GOOGLEANALYTICS_EDIT_RULE');
-
-						// Load the edit rule handle in the admin controller
-						$admin_controller->edit_rule($rule_id);
-
-						// Return to stop execution of this script
-						return;
-					break;
-
-					case 'move_down':
-						// Move a rule down one position
-						$admin_controller->move_rule($rule_id, 'down');
-					break;
-
-					case 'move_up':
-						// Move a rule up one position
-						$admin_controller->move_rule($rule_id, 'up');
-					break;
-
-					case 'delete':
-						// Delete a rule
-						$admin_controller->delete_rule($rule_id);
-					break;
-				}
-
-				// Check if a language variable was submitted and display
-				// the rules for that language. If no language was submitted,
-				// display the language selection menu.
-				if (empty($language))
-				{
-					$admin_controller->display_language_selection();
-				}
-				else
-				{
-					$admin_controller->display_rules($language, $parent_id);
-				}
-			break;
 		}
 	}
 }
