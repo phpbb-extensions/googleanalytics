7ed748deaed59600fbc6111ba93199b1a45fd022
Michael O'Toole <phpbbireland@gmail.com>
2014-07-08 02:51:31 +0100

    Add permissions to migration

--- migrations/v10x/m1_initial_data.php
+++ migrations/v10x/m1_initial_data.php
@@ -15,7 +15,14 @@ class m1_initial_data extends \phpbb\db\migration\migration
 	public function update_data()
 	{
 		return array(
+			// Add config variable
 			array('config.add', array('googleanalytics', '')),
+
+			// Add permission
+			array('permission.add', array('a_googleanalytics', true)),
+
+			// Set permissions
+			array('permission.permission_set', array('ROLE_ADMIN_FULL', 'a_googleanalytics')),
 		);
 	}
 }
