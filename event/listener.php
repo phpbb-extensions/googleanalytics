f66ec453d42e41b9c3de9a27354bd47537136b59
Pico88 <luki_9@wp.pl>
2014-07-14 20:14:12 +0200

    Add event listner

--- event/listener.php
+++ event/listener.php
@@ -17,4 +17,52 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\config\config        $config             Config object
+	* @param \phpbb\template\template    $template           Template object
+	* @return \phpbb\boardrules\event\listener
+	* @access public
+	*/
+	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template)
+	{
+		$this->config = $config;
+		$this->template = $template;
+	}
+
+	/**
+	* Assign functions defined in this class to event listeners in the core
+	*
+	* @return array
+	* @static
+	* @access public
+	*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.common'	=> 'load_google_analytics',
+		);
+	}
+
+	/**
+	* Load Google Analytics js code
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function load_google_analytics($event)
+	{
+		$this->template->assign_vars(array(
+			'S_GOOGLE_ANALYTICS_ENABLE'	=> (!empty($this->config['googleanalytics'])) ? true : false,
+			'GOOGLE_ANALYTICS_ID'		=> $this->config['googleanalytics'],
+		));
+	}
 }
