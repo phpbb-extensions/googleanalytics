93b9ef102531341f8696e48a11d1da993c307e04
Matt Friedman <maf675@gmail.com>
2021-06-10 11:27:57 -0700

    Drop support for phpBB 3.1 and modernize code

---
+++ ext.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Google Analytics extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2021 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace phpbb\googleanalytics;
+
+use phpbb\extension\base;
+
+class ext extends base
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function is_enableable()
+	{
+		return phpbb_version_compare(PHPBB_VERSION, '3.2.0', '>=');
+	}
+}
--- migrations/v10x/m1_initial_data.php
+++ migrations/v10x/m1_initial_data.php
@@ -1,42 +1,42 @@
 <?php
 /**
-*
-* Google Analytics extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * Google Analytics extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
 namespace phpbb\googleanalytics\migrations\v10x;
 
 /**
-* Migration stage 1: Initial data changes to the database
-*/
+ * Migration stage 1: Initial data changes to the database
+ */
 class m1_initial_data extends \phpbb\db\migration\migration
 {
 	/**
-	* Assign migration file dependencies for this migration
-	*
-	* @return array Array of migration files
-	* @static
-	* @access public
-	*/
+	 * Assign migration file dependencies for this migration
+	 *
+	 * @return array Array of migration files
+	 * @static
+	 * @access public
+	 */
 	public static function depends_on()
 	{
-		return array('\phpbb\db\migration\data\v310\gold');
+		return ['\phpbb\db\migration\data\v310\gold'];
 	}
 
 	/**
-	* Add Google Analytics data to the database.
-	*
-	* @return array Array of table data
-	* @access public
-	*/
+	 * Add Google Analytics data to the database.
+	 *
+	 * @return array Array of table data
+	 * @access public
+	 */
 	public function update_data()
 	{
-		return array(
-			array('config.add', array('googleanalytics_id', '')),
-		);
+		return [
+			['config.add', ['googleanalytics_id', '']],
+		];
 	}
 }
